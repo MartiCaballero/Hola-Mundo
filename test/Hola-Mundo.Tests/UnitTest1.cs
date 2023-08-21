a0352a380382a36955ca74ff808041571a074dc0
MartiCaballero <martucaballero93@gmail.com>
2023-08-21 20:55:59 -0300

    proyecto

---
+++ test/Hola-Mundo.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Hola_Mundo.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
